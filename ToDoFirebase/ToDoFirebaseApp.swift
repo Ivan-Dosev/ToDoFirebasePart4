43ff2c5bc01599c802286b6e5308a4a818190942
Ivan Dosev <biobeehive@abv.bg>
2021-02-05 19:02:25 +0100

    Initial Commit

---
+++ ToDoFirebase/ToDoFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoFirebaseApp.swift
+//  ToDoFirebase
+//
+//  Created by Ivan Dimitrov on 5.02.21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
